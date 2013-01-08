7e82145f9a477a1eb69cfd262c40fae7beb0c3c8
Eric Lombrozo <elombrozo@gmail.com>
2013-01-08 03:42:22 -0800

    Moved CInPoint to core. Removed GetMinFee from CTransaction and made it a regular function in main.
    Conflicts:
    	src/main.cpp
    	src/main.h

--- src/main.h
+++ src/main.h
@@ -266,20 +266,6 @@ struct CDiskTxPos : public CDiskBlockPos
 };
 
 
-/** An inpoint - a combination of a transaction and an index n into its vin */
-class CInPoint
-{
-public:
-    CTransaction* ptx;
-    unsigned int n;
-
-    CInPoint() { SetNull(); }
-    CInPoint(CTransaction* ptxIn, unsigned int nIn) { ptx = ptxIn; n = nIn; }
-    void SetNull() { ptx = NULL; n = (unsigned int) -1; }
-    bool IsNull() const { return (ptx == NULL && n == (unsigned int) -1); }
-};
-
-
 
 
 /** An input of a transaction.  It contains the location of the previous
@@ -434,6 +420,8 @@ enum GetMinFee_mode
     GMF_RELAY,
     GMF_SEND,
 };
+
+int64 GetMinFee(const CTransaction& tx, unsigned int nBlockSize = 1, bool fAllowFree = true, enum GetMinFee_mode mode = GMF_BLOCK);
 
 /** The basic transaction that is broadcasted on the network and contained in
  * blocks. A transaction can contain multiple inputs and outputs.
@@ -598,8 +586,6 @@ public:
 
 // Apply the effects of this transaction on the UTXO set represented by view
 void UpdateCoins(const CTransaction& tx, CValidationState &state, CCoinsViewCache &inputs, CTxUndo &txundo, int nHeight, const uint256 &txhash);
-
-    int64 GetMinFee(unsigned int nBlockSize=1, bool fAllowFree=true, enum GetMinFee_mode mode=GMF_BLOCK) const;
 
     friend bool operator==(const CTransaction& a, const CTransaction& b)
     {
