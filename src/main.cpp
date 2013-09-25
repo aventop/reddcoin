76dbf788e365b78f8729de1c56001ce534262bef
Philip Kaufmann <phil.kaufmann@t-online.de>
2013-09-25 15:45:46 +0200

    internal miner: move 2 globals from main to miner
    - moves 2 global variables from main.cpp/h to miner.cpp/h
    - also removes 2 unneded includes in miner.cpp, that come from miner.h
      already

--- src/main.cpp
+++ src/main.cpp
@@ -71,9 +71,6 @@ CScript COINBASE_FLAGS;
 
 const string strMessageMagic = "Reddcoin Signed Message:\n";
 
-double dHashesPerSec = 0.0;
-int64 nHPSTimerStart = 0;
-
 // Settings
 int64 nTransactionFee = 0;
 int64 nMinimumInputValue = DUST_HARD_LIMIT;
